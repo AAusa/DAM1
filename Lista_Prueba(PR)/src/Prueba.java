15732019a323611e23f18444a2ae97f19203de4b
aa21.aausaberri87@iespabloserrano.com <aa21.aausaberri87@iespabloserrano.com>
2022-03-22 14:22:29 +0100

    bubble

--- Lista_Prueba(PR)/src/Prueba.java
+++ Lista_Prueba(PR)/src/Prueba.java
@@ -6,10 +6,11 @@ public class Prueba {
 		int ordenado = 0;
 		int contador = 0;
 		int cantidad = 3;
-		int [] lista = new int [5];
+		int [] lista = new int [3];
 		lista[0] = 6;
 		lista[1] = 17;
 		lista[2] = 4;
+		
 		/**
 		while(lista[aOrdenar] > lista[ordenado]) {
 			//Encontrar elemento aOrdenar:
@@ -49,32 +50,117 @@ public class Prueba {
 			//insercion:
 			lista [p] = x;
 		}*/
+		/*
 		int menor;
 		int contenidoMenor = 0;
 		cantidad = 5;
-		for(int k = 0; k < cantidad; k++) {
-			menor = k;
+		for(int i = 0; i < cantidad; i++) {
+			menor = i;
 			//1º Seleccion del menor
-			for(int i = k+1; i < cantidad; i ++) {
-				if(lista[i] < lista[menor]) {
-					menor = i;
+			for(int j = i+1; j < cantidad; j ++) {
+				if(lista[j] < lista[menor]) {
+					menor = j;
 				}	
 			}
-			if(k != menor) {
+			if(i != menor) {
+				//Guardar contenido del menor en variable:
 				lista[menor] = contenidoMenor;
-				/*
-				//2º Correr posiciones entre la 0 y la menor
-				for(int j = 1; j < menor; j++) {
-					lista[j] = lista[j-1];
-				}*/
-				lista[menor] = lista[k];
-				//3º Inserto la menor
-				lista[k] = contenidoMenor;
+				// Intercambio de valores entre lista[menor] y lista[i]
+				lista[menor] = lista[i];
+				lista[i] = contenidoMenor;
 			}
 		}
-			
+		*/	
+		/*
+		boolean ordenado;
+		int elMenor = 0;
 		for(int i = 0; i < cantidad; i++) {
 			System.out.println(lista[i]);
 		}
-	}
+		System.out.println();
+		for(int i = 0; i < cantidad - 1; i ++) {
+			ordenado = true;
+			for(int j = 0; j < cantidad - (i + 1); j++) {
+				if(lista[j+1] < lista[j]) {
+					ordenado = false;
+					lista[j+1] = elMenor;
+					lista[j+1] = lista[j];
+					lista[j] = elMenor;
+				}
+			}
+			// if (ordenado)
+		}
+		
+		boolean ordenado;
+		int elMenor = 0;
+		for(int i = 0; i < cantidad; i++) {
+			System.out.println(lista[i]);
+		}
+		System.out.println();
+		for(int i = 0; i < cantidad - 1; i ++) {
+			for(int j = cantidad - 1; j <= i; i++) {
+				if(lista[j+1] < lista[j]) {
+					ordenado = false;
+					lista[j+1] = elMenor;
+					lista[j+1] = lista[j];
+					lista[j] = elMenor;
+				}
+			}
+		}*/
+		int elMenor = 0;
+		boolean ordenadoTodo = false;
+		int i = 0;
+		do {
+			//Pasadas pares ordena en un sentido:
+			if(i == 0 | i % 2 == 0) {
+				for(int j = 0; j < cantidad - (j + 1); j++) {
+					if(lista[j+1] < lista[j]) {
+						ordenadoTodo = false;
+						lista[j+1] = elMenor;
+						lista[j+1] = lista[j];
+						lista[j] = elMenor;
+					}
+				}
+			}
+			//Pasadas impares ordena en un sentido:
+			else {
+				for(int j = cantidad - 1; j <= i; i++) {
+					if(lista[j+1] < lista[j]) {
+						ordenadoTodo = false;
+						lista[j+1] = elMenor;
+						lista[j+1] = lista[j];
+						lista[j] = elMenor;
+					}
+				}
+			}
+		}
+			while(ordenadoTodo = false);
+	
+		int u = cantidad - 1;
+		int p = 0;
+		do {
+			for(int j = u; j <= p; j--) {
+				if(lista[j+1] < lista[j]) {
+					ordenadoTodo = false;
+					lista[j+1] = elMenor;
+					lista[j+1] = lista[j];
+					lista[j] = elMenor;
+				}			
+			}
+			p++;
+			for(int j = p; j <= u; j++) {
+				if(lista[j+1] < lista[j]) {
+					ordenadoTodo = false;
+					lista[j+1] = elMenor;
+					lista[j+1] = lista[j];
+					lista[j] = elMenor;
+				}
+			u--;
+		}
+		while (p < u);
+		for(int x = 0; x < cantidad; x++) {
+			System.out.println(lista[x]);
+		}
+	
+}
 }
